19ab10b11e995129b98ed680a5e9a6da7a29a741
lixinkuan <844233831@qq.com>
2024-10-13 20:12:39 +0800

    解决找不到文件

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
+import { RouterView } from "vue-router";
 import Header from "./views/header/header.vue";
 import Footer from "./views/footer/footer.vue";
 </script>
