aec89061b74d70c9404fa8806774cdc6eec0adf7
lixinkuan <844233831@qq.com>
2024-10-13 20:01:41 +0800

    zhengli

--- src/App.vue
+++ src/App.vue
@@ -12,33 +12,6 @@ import Footer from "./views/footer/footer.vue";
 </template>
 
 <style>
-/* 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-} */
 a {
   text-decoration: none;
   color: #1b242b;
